f40d1397abb4bb7f68d7caeab32dc15cfa71128f
KaitoHH <hh.kaito@gmail.com>
2017-06-27 17:12:17 +0800

    feat: complete regularized logistic regression for ex2

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -16,11 +16,13 @@ grad = zeros(size(theta));
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
+n = length(theta);
 
+h = sigmoid(X * theta);
+J = 1 / m * (-y' * log(h) - (1 - y)' * log(1 - h)) + lambda / (2 * m) * sum((theta .^ 2)(2:n));
 
-
-
-
+grad = 1 / m * X' * (h - y);
+grad(2:n)+= lambda / m * theta(2:n);
 
 % =============================================================
 
